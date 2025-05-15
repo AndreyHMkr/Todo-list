4ff8991794af2d8cfa682699d5a06fdfd968a8e6
Andriy <grushevoyandrey@gmail.com>
2025-05-15 19:44:57 +0100

    Add: Task and Tag models with fields and relationships

--- catalog/models.py
+++ catalog/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
+
 class Task(models.Model):
     content = models.TextField(verbose_name="Task Description")
-    created_ad = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
     deadline = models.DateTimeField(blank=True, null=True, verbose_name="Deadline")
     task_is_done = models.BooleanField(default=False, verbose_name="Task is Done")
     tags = models.ManyToManyField("Tag", related_name="tasks", verbose_name="Tags")
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
